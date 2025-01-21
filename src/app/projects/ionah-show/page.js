fa0520e16970747b3723843c0ae3248bb2037680
23nhannt <nguyentiennhan9a2@gmail.com>
2025-01-22 00:41:59 +0700

    done Desc in Ionah

--- src/app/projects/ionah-show/page.js
+++ src/app/projects/ionah-show/page.js
@@ -1,8 +1,14 @@
-import ProjectIntro from "../central-circus/projectIntro";
+import ProjectIntro from "./projectIntro";
+import ProjectDesc from "./projectDesc";
+import Testimony from "./testimony";
 
 export default function IonahShow() {
     return (
-      <ProjectIntro />
+      <>
+        <ProjectIntro />
+        <ProjectDesc />
+        <Testimony />
+      </>
     );
   }
   
